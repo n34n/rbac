cda549deef4bd97ffcc0e72cac4429f6b88043e0
n34n <34n@163.com>
2016-08-18 18:58:33 +0800

    left side menu actived fixed

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -31,55 +31,27 @@ use mdm\admin\components\MenuHelper;
         </form>
         <!-- /.search form -->
 
-
-
-        <?= dmstr\widgets\Menu::widget(
-            [
-                'options' => ['class' => 'sidebar-menu'],
-                'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Same tools',
-                        'icon' => 'fa fa-share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'fa fa-circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'fa fa-circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'fa fa-circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ]
-        ) ?>
-
 <?php
 $callback = function($menu){
     $data = json_decode($menu['data'], true);
     $items = $menu['children'];
-    $return = [
-        'label' => $menu['name'],
-        'url' => [$menu['route']],
-    ];
-    //处理我们的配置
+    $_actived = explode('/', Yii::$app->controller->route);
+    $_route   = explode('/', substr($menu['route'],1));
+
+    if($_actived[0] == $_route[0]){
+        $return = [
+            'label' => $menu['name'],
+            'url' => [$menu['route']],
+            'active' => 1,
+        ];
+    }else{
+        $return = [
+            'label' => $menu['name'],
+            'url' => [$menu['route']],
+        ];        
+    }
+
+
     if ($data) {
         //visible
         isset($data['visible']) && $return['visible'] = $data['visible'];
@@ -88,7 +60,7 @@ $callback = function($menu){
         //other attribute e.g. class...
         $return['options'] = $data;
     }
-    //没配置图标的显示默认图标
+
     (!isset($return['icon']) || !$return['icon']) && $return['icon'] = 'fa fa-circle-o';
     $items && $return['items'] = $items;
     return $return;
